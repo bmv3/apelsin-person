c6c0b7429feed23f67be1f9626e60e8281553a70
Michael V. Bykov <bmv3@mail.ru>
2023-08-17 00:52:57 +0500

    Small corrections

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8d1a70ed46ffff63b8e3');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '658d36ae86f7b2b51d16');
